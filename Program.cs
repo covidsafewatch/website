8721e70c9341d3761c0e9571d3ea84cc292dce54
Geoffrey Huntley <ghuntley@ghuntley.com>
2020-05-12 20:47:45 +0000

    wip

--- Program.cs
+++ Program.cs
@@ -2,24 +2,37 @@
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Octokit;
 using Statiq.App;
 using Statiq.Common;
 using Statiq.Web;
 
 namespace CovidSafeWatch
 {
-  public class Program
-  {
-    public static async Task<int> Main(string[] args) =>
-      await Bootstrapper
-        .Factory
-        .CreateWeb(args)
-        .AddSetting(Keys.Host, "covidsafe.watch")
-        .AddSetting(WebKeys.ValidateAbsoluteLinks, true)
-        .AddSetting(Keys.LinksUseHttps, true)
-        .AddSetting(WebKeys.MirrorResources, true)
-        .AddSetting("EditLink", Config.FromDocument((doc, ctx) => "https://github.com/covidsave-watch/website/edit/master/input/" + doc.Source.GetRelativeInputPath()))
-        .AddPipelines()
-        .RunAsync();
-  }
+    public static class Program
+    {
+        public static async Task<int> Main(string[] args) =>
+            await Bootstrapper.Factory
+                .CreateWeb(args)
+                .AddSetting(Keys.Host, "covidsafe.watch")
+                .AddSetting(Keys.LinksUseHttps, true)
+                .AddSetting(WebKeys.MirrorResources, true)
+                .AddSetting(
+                    Keys.DestinationPath,
+                    Config.FromDocument((doc, ctx) =>
+                    {
+                        // Only applies to the content pipeline
+                        if (ctx.PipelineName == nameof(Statiq.Web.Pipelines.Content))
+                        {
+                            return doc.Source.Parent.Segments.Last().SequenceEqual("posts".AsMemory())
+                                ? new NormalizedPath("blog").Combine(doc.GetDateTime(WebKeys.Published).ToString("yyyy/MM/dd")).Combine(doc.Destination.FileName.ChangeExtension(".html"))
+                                : doc.Destination.ChangeExtension(".html");
+                        }
+                        return doc.Destination;
+                    }))
+                .AddSetting("EditLink", Config.FromDocument((doc, ctx) => "https://github.com/covidsafewatch/website/edit/develop/input/" + doc.Source.GetRelativeInputPath()))
+                .AddSetting(SiteKeys.NoChildPages, Config.FromDocument(doc => doc.Destination.Segments[0].SequenceEqual("blog".AsMemory())))
+                .AddPipelines()
+                .RunAsync();
+    }
 }
---
+++ SectionNames.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CovidSafeWatch
+{
+    public static class SectionNames
+    {
+        public const string Head = nameof(Head);
+        public const string Scripts = nameof(Scripts);
+        public const string Splash = nameof(Splash);
+        public const string Sidebar = nameof(Sidebar);
+        public const string Subtitle = nameof(Subtitle);
+    }
+}
