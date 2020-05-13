44b5dca67b065354cf4815c6c1d8be7f89e783d7
Geoffrey Huntley <ghuntley@ghuntley.com>
2020-05-13 10:03:21 +0000

    a

--- SiteKeys.cs
+++ SiteKeys.cs
@@ -12,5 +12,14 @@ namespace CovidSafeWatch
         public const string Topic = nameof(Topic);
         public const string NoChildPages = nameof(NoChildPages);
         public const string BreadcrumbTitle = nameof(BreadcrumbTitle); // The title of a page when displayed in the breadcrumbs
+
+        public const string BreachesPrivacy = nameof(BreachesPrivacy); // true || false
+        public const string DisclosedAt = nameof(DisclosedAt); // datetime
+        public const string ResolvedAt = nameof(ResolvedAt); // datetime
+        public const string JiraCaseNumber = nameof(JiraCaseNumber); 
+        public const string CVEIdentifier = nameof(CVEIdentifier);
+        public const string Status = nameof(Status); // Unresolved, Acknowledged, Resolved, Invalid
+        public const string IssueMaintainer = nameof(IssueMaintainer); // github username of the person who is maintainer of the issue 
+
     }
 }
